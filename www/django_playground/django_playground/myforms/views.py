aa8a95556c39e45c5e4c5312eae13cd6d3e1d94f
osama <osamana@outlook.com>
2017-01-11 15:28:18 +0200

    forms v2

--- www/django_playground/django_playground/myforms/views.py
+++ www/django_playground/django_playground/myforms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.mail import send_mail
 
 from .forms import NameForm
 
@@ -13,9 +14,18 @@ def get_name(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+
+            recipients = ['info@example.com']
+            if cc_myself:
+                recipients.append(sender)
+
+            send_mail(subject, message, sender, recipients)
             return HttpResponseRedirect(reverse('myforms:thanks'))
+            # redirect to a new URL:
 
     # if a GET (or any other method) we'll create a blank form
     else:
